c2479a4406f43092254b4fa1e39fbbd3dd4a7f6f
John Page <jdehpage@gmail.com>
2019-04-12 21:55:56 +0100

    Amenity class done

--- PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/CustomerBooking.java
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/CustomerBooking.java
@@ -1,4 +1,28 @@
 package com.propertyproperly.codeclan.PropertyProperlyService.models;
 
 public class CustomerBooking extends Booking {
+
+    private Boolean paid;
+    private Customer customer;
+
+    public CustomerBooking(String startDate, String endDate, BookableItem bookableItem, Boolean ongoing, Boolean paid) {
+        super(startDate, endDate, bookableItem, ongoing);
+        this.paid = paid;
+    }
+
+    public Boolean getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Boolean paid) {
+        this.paid = paid;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
