2a683755195b88c71ad81198797a7d23ed0b5f51
John Page <jdehpage@gmail.com>
2019-04-12 21:40:58 +0100

    BookableItemType Done

--- PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/BookingItemType.java
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/BookingItemType.java
@@ -1,4 +1,18 @@
 package com.propertyproperly.codeclan.PropertyProperlyService.models;
 
 public class BookingItemType {
+
+    private String name;
+
+    public BookingItemType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
