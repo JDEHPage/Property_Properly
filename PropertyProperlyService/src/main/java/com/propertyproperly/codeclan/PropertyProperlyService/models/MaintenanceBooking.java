a7809d3924aadb003e7aff23a307d806fd3404e4
John Page <jdehpage@gmail.com>
2019-04-12 22:04:07 +0100

    MaintenanceBooking module created

--- PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/MaintenanceBooking.java
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/MaintenanceBooking.java
@@ -1,4 +1,19 @@
 package com.propertyproperly.codeclan.PropertyProperlyService.models;
 
 public class MaintenanceBooking extends Booking {
+
+    private String type;
+
+    public MaintenanceBooking(String startDate, String endDate, BookableItem bookableItem, Boolean ongoing, String type) {
+        super(startDate, endDate, bookableItem, ongoing);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
