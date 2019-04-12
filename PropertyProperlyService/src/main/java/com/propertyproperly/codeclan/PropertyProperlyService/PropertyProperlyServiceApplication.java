adff409f2b0215f79dabc2afe285678f7ea331d8
John Page <jdehpage@gmail.com>
2019-04-12 17:49:20 +0100

    Java propertied added

---
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/PropertyProperlyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertyproperly.codeclan.PropertyProperlyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyProperlyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyProperlyServiceApplication.class, args);
+	}
+
+}
---
+++ PropertyProperlyService/src/test/java/com/propertyproperly/codeclan/PropertyProperlyService/PropertyProperlyServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.propertyproperly.codeclan.PropertyProperlyService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertyProperlyServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
