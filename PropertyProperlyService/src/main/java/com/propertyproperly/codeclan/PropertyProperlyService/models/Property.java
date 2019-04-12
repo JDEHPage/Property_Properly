c74c97ee657502a591291b3aa19a9d902b688dca
Stuart Bell <stuart.bell1982@googlemail.com>
2019-04-12 19:08:43 +0100

    added Propery class

--- PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/Property.java
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/Property.java
@@ -1,4 +1,35 @@
 package com.propertyproperly.codeclan.PropertyProperlyService.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Property {
+
+    private String name;
+    private List<BookableItems> bookableItems;
+
+    public Property(String name) {
+        this.name = name;
+        this.bookableItems = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BookableItems> getBookableItems() {
+        return bookableItems;
+    }
+
+    public void setBookableItems(List<BookableItems> bookableItems) {
+        this.bookableItems = bookableItems;
+    }
+
+    public void addBooking(BookableItems bookableItems){
+        this.bookableItems.add(bookableItems);
+    }
 }
