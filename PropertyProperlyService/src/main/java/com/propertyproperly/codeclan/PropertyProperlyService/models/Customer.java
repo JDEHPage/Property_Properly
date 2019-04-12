c727ed5c9d2b632f8bf7d49c88abfd9c7ad56caf
Stuart Bell <stuart.bell1982@googlemail.com>
2019-04-12 19:02:06 +0100

    customer class done

--- PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/Customer.java
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/Customer.java
@@ -1,4 +1,48 @@
 package com.propertyproperly.codeclan.PropertyProperlyService.models;
 
 public class Customer {
+
+    private String name;
+    private String address;
+    private String email;
+    private int mobileNo;
+
+    public Customer(String name, String address, String email, int mobileNo) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.mobileNo = mobileNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
 }
