1e19384c9ad9930412b48c7d4c54b19383bdfdd2
John Page <jdehpage@gmail.com>
2019-04-12 22:02:03 +0100

    PaymentOption module created

---
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/PaymentOption.java
@@ -0,0 +1,28 @@
+package com.propertyproperly.codeclan.PropertyProperlyService.models;
+
+public class PaymentOption {
+
+    private String name;
+    private String description;
+
+    public PaymentOption(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
