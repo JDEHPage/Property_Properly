ec07e3f9e57075a3ff920785b9a40bb42fd01520
carmemias <info@carmemias.com>
2019-04-14 21:54:36 +0100

    Created BookeableItemType

---
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/models/BookableItemType.java
@@ -0,0 +1,54 @@
+package com.propertyproperly.codeclan.PropertyProperlyService.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "bookable-item-types")
+public class BookableItemType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    @OneToMany(mappedBy = "type", fetch = FetchType.LAZY)
+    private List<BookableItem> bookableItemsList;
+
+    public BookableItemType(String name) {
+        this.name = name;
+    }
+
+    public BookableItemType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BookableItem> getBookableItemsList() {
+        return bookableItemsList;
+    }
+
+    public void setBookableItemsList(List<BookableItem> bookableItemsList) {
+        this.bookableItemsList = bookableItemsList;
+    }
+}
---
+++ property_properly/src/containers/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+class Main extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render (){
+		return(
+			<Router>
+
+			</Router>
+		);
+	}
+}
+
+export default Main;
