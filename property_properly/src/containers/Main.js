9534ecff70791bf55d027196faa9f9738bf6ef7f
carmemias <info@carmemias.com>
2019-04-16 07:04:40 +0100

    Initial Main state and api data fetch

--- property_properly/src/containers/Main.js
+++ property_properly/src/containers/Main.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from '../components/NavBar';
-import FrontDesk from '../components/FrontDesk';
-import Bookings from '../components/Bookings';
-import Customers from '../components/Customers';
-import Availability from '../components/Availability';
-import Admin from '../components/Admin';
+import NavBar from "../components/NavBar";
+import FrontDesk from "../components/FrontDesk";
+import Bookings from "../components/Bookings";
+import Customers from "../components/Customers";
+import Availability from "../components/Availability";
+import Admin from "../components/Admin";
+import ErrorPage from "../components/ErrorPage";
 
 class Main extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-
+			bookings: [],
+			customers: []
 		}
+	}
+
+	componentDidMount(){
+		fetch('http://localhost:8080/api/bookings')
+		.then(res => res.json())
+		.then(data => {
+			this.setState( { bookings: data } );
+		});
+
+		fetch('http://localhost:8080/api/customers')
+		.then(res => res.json())
+		.then(data => {
+			this.setState( { customers: data } );
+		});
 	}
 
 	render (){
@@ -25,6 +41,7 @@ class Main extends Component{
 					<Route exact path="/customers" component={Customers} />
 					<Route exact path="/availability" component={Availability} />
 					<Route exact path="/admin" component={Admin} />
+					<Route component={ErrorPage}/>
 				</Switch>
 			</Router>
 		);
