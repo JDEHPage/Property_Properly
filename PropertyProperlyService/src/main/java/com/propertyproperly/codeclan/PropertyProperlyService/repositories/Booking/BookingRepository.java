df1656c852bf20e814c50739b7b824047b93ffea
carmemias <info@carmemias.com>
2019-04-16 14:13:55 +0100

    Fixed booking repository

--- PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/repositories/Booking/BookingRepository.java
+++ PropertyProperlyService/src/main/java/com/propertyproperly/codeclan/PropertyProperlyService/repositories/Booking/BookingRepository.java
@@ -1,9 +1,6 @@
 package com.propertyproperly.codeclan.PropertyProperlyService.repositories.Booking;
-
 import com.propertyproperly.codeclan.PropertyProperlyService.models.Booking;
-import com.propertyproperly.codeclan.PropertyProperlyService.projections.EmbedBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
