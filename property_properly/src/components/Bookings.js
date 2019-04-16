d66923b5ef921cbe2710f6815b70554ac89f8696
carmemias <info@carmemias.com>
2019-04-16 07:20:09 +0100

    Start bookings and customer tables

--- property_properly/src/components/Bookings.js
+++ property_properly/src/components/Bookings.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Bookings.css';
 
 const Bookings = () => {
 	return (
 		<main>
 		<h2 className="page-title">Bookings</h2>
+
+		<table className="bookings-table" cellpadding="5">
+			<tr>
+				<th>ID</th>
+				<th>Start Date</th>
+				<th>End Date</th>
+				<th>Nights</th>
+				<th>Customer</th>
+				<th>Room</th>
+				<th>Total Price</th>
+			</tr>
+			<tr>
+			</tr>
+		</table>
 		</main>
 	);
 }
--- property_properly/src/components/Customers.js
+++ property_properly/src/components/Customers.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Customers.css';
 
 const Customers = () => {
 	return(
 		<main>
 		<h2 className="page-title">Customers</h2>
+
+		<table className="customers-table" cellpadding="5">
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Address</th>
+				<th>Email</th>
+				<th>Telephone</th>
+				<th>Num. Bookings</th>
+			</tr>
+
+		</table>
 		</main>
 	);
 }
