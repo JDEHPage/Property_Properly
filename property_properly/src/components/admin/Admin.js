8c96f4b5d88b096fb48958a50c69f866f8882016
John Page <jdehpage@gmail.com>
2019-04-17 16:24:02 +0100

    Admin froms added

--- property_properly/src/components/admin/Admin.js
+++ property_properly/src/components/admin/Admin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropertiesForm from '../properties/PropertiesForm.js'
 import TypesForm from '../types/TypesForm.js'
+import ItemsForm from '../items/ItemsForm.js'
 
 const Admin = (props) => {
 		return(
@@ -8,6 +9,9 @@ const Admin = (props) => {
 			<h2 className="page-title">Admin Dashboard</h2>
 			<PropertiesForm properties={props.properties}/>
 			<TypesForm bookableItemTypes={props.bookableItemTypes}/>
+			<ItemsForm bookableItems={props.bookableItems}
+			properties={props.properties}
+			bookableItemTypes={props.bookableItemTypes}/>
 			</main>
 		);
 	}
