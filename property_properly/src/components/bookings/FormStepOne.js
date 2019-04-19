cc430e63c37d64fba3545e2ffe9f48d574651a70
carmemias <info@carmemias.com>
2019-04-19 07:17:27 +0100

    Conditional activation of Next Button for stepOne

--- property_properly/src/components/bookings/FormStepOne.js
+++ property_properly/src/components/bookings/FormStepOne.js
@@ -1,21 +1,44 @@
 import React, {Component} from 'react'
 
-const FormStepOne = (props) => {
+class FormStepOne extends Component {
 
-	const handleDateChange = (event) => {
-		props.handleChange(event)
+	constructor(props){
+		super(props);
+		this.state = {
+			startDate: '',
+			endDate: '',
+			roomSelected: false,
+			stepCompleted: false
+		}
+		this.handleDateChange = this.handleDateChange.bind(this);
+		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+		this.itemlist = this.itemlist.bind(this);
+		this.stepOneNext = this.stepOneNext.bind(this);
 	}
 
-	const handleCheckboxChange = (event) => {
-		props.handleCheckboxChange(event)
+	handleDateChange(event){
+		this.setState({[event.target.name]: event.target.value})
+		if(event.target.name === "startDate"){
+			this.setState({stepCompleted: (event.target.value !== "") && (this.state.endDate !== "") && this.state.roomSelected })
+		} else {
+			this.setState({stepCompleted: (this.state.startDate !== "") && (event.target.value !== "") && this.state.roomSelected })	
+		}
+		this.props.handleChange(event)
 	}
 
-	const itemlist = (bookableItems) => {
+	handleCheckboxChange(event){
+		this.setState({roomSelected: event.target.checked})
+		this.setState({stepCompleted: (this.state.startDate !== "") && (this.state.endDate !== "") && event.target.checked })
+
+		this.props.handleCheckboxChange(event)
+	}
+
+	itemlist(bookableItems){
 		const result = bookableItems.map((item, index) => {
 			const id = `cust${index}`;
 			return (
 				<span key={index}>
-				<input type="checkbox" name="bookableItems" id={id}  value={item._links.self.href} onChange={handleCheckboxChange}/>
+				<input type="checkbox" name="bookableItems" id={id}  value={item._links.self.href} onChange={this.handleCheckboxChange}/>
 				<label htmlFor={id} >{item.name} </label>
 				</span>
 			)
@@ -23,20 +46,7 @@ const FormStepOne = (props) => {
 		return result;
 	}
 
-	const disabled = () => {
-		const startDate = document.getElementById('startDate');
-		// const endDate = document.getElementById('endDate');
-		// const rooms = document.querySelectorAll('[type="checkbox"]');
-
-		console.log(startDate.value);
-		if (startDate.value !== ""){
-			return false
-		} else {
-			return true
-		}
-	}
-
-	const stepOneNext = (event) => {
+	stepOneNext(event){
 		event.preventDefault();
 
 		let stepOneDiv = document.getElementById("stepOne");
@@ -46,28 +56,29 @@ const FormStepOne = (props) => {
 		stepTwoDiv.style.cssText="opacity: 1; height: 350px;";
 	}
 
+	render(){
 	return(
 		<div id="stepOne">
 
 		<label htmlFor="startDate">Start Date: &nbsp;
-		<input type = "date" id="startDate" name="startDate" onChange={handleDateChange}/>
+		<input type = "date" id="startDate" name="startDate" onChange={this.handleDateChange}/>
 		</label>
 
 		<label htmlFor="endDate">End Date: &nbsp;
-		<input type = "date" id="endDate" name="endDate" onChange={handleDateChange}/>
+		<input type = "date" id="endDate" name="endDate" onChange={this.handleDateChange}/>
 		</label>
 
 		<fieldset >
 			<legend>Select Rooms</legend>
-			{itemlist(props.bookableItems)}
+			{this.itemlist(this.props.bookableItems)}
 		</fieldset>
 
 		<div className="nav-btns">
-			<button className="next" onClick={stepOneNext} disabled={disabled}> &gt;&gt; </button>
+			<button className="next" onClick={this.stepOneNext} disabled={!this.state.stepCompleted}> &gt;&gt; </button>
 		</div>
 
 		</div>
-	)
+	)}
 
 }
 
